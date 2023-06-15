331e63a663a0389511529eedbab71775024094d4
Camron Naderi <camronnaderi57@gmail.com>
2023-06-14 17:11:37 -0700

    add new styling for entire website + add most html and css for our new tweet

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('#tweet-text').on('input', function() {
+    const counter = $('.counter');
+    const remainingChars = 140 - $(this).val().length;
+    counter.text(remainingChars);
+
+    if (remainingChars < 0) {
+      counter.addClass('red-text');
+    } else {
+      counter.removeClass('red-text');
+    }
+  });
+});
+
+
